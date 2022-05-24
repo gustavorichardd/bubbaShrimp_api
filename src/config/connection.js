6f72da12ecf2ff19199ef66f6eb22cc0a56d7b20
Gustavo Richard <ogustavorichard@gmail.com>
2022-05-24 11:20:13 -0300

    .

--- src/config/connection.js
+++ src/config/connection.js
@@ -8,25 +8,6 @@ const sequelize = new Sequelize({
    password: process.env.BUBBA_PASSWORD,
    host: process.env.BUBBA_HOST,
    port: process.env.BUBBA_PORT,
-   // dialectOptions: {
-   //    ssl: {
-   //       require: true,
-   //       rejectUnauthorized: false
-   //    }
-   // },
 });
-
-// const sequelize = new Sequelize(process.env.BUBBA_DB, process.env.BUBBA_USER, process.env.BUBBA_PASSWOR, {
-//    host: '127.0.0.1',
-//    dialect: 'mariadb'
-// });
-
-
-// try {
-//    sequelize.authenticate().then(() => console.log('Connection has been established successfully.'));
-
-// } catch (error) {
-//    console.error('Unable to connect to the database:', error);
-// }
 
 module.exports = sequelize;
